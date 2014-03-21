3e1b07baf3f98a04b6407673022f69c41c2b058e
oliverwreath <skywalkerhunter@gmail.com>
2014-03-20 22:38:34 -0700

    Threading, Socket Programming - ChatRoom

---
+++ Engineering-Java/workspace/com.java/src/com/java/chatroom/basic2threading/CloseUtil.java
@@ -0,0 +1,19 @@
+package com.java.chatroom.basic2threading;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+public class CloseUtil {
+	public static void closeAll(Closeable... io){
+		for( Closeable temp :io  ){
+			if( temp != null ){
+				try {
+					temp.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
