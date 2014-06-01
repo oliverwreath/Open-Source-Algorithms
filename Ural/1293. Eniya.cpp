03ef8a01a7f6da482b05806a858972e4c72d5e2b
oliverwreath <oliverwreath@users.noreply.github.com>
2014-06-01 21:41:51 +0800

    Create 1293. Eniya.cpp

---
+++ Ural/1293. Eniya.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cassert>
+#include <sstream>
+#include <algorithm>
+#include <unordered_set>
+#include <cstdio>
+#include <cstring>
+#include <climits>
+#include <cfloat>
+#include <cmath>
+#include <stack>
+
+using namespace std;
+
+int main(){
+	int n, a, b;
+	cin >> n >> a >> b;
+	cout << 2 * n * a * b << endl;
+
+	return 0;
+}
