c4e68461d0a58d5a9ff77e06fd56b6bc001bb89e
oliverwreath <oliverwreath@users.noreply.github.com>
2014-06-01 21:48:13 +0800

    Create 1264. Workdays.cpp

---
+++ Ural/1264. Workdays.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cassert>
+#include <sstream>
+#include <algorithm>
+#include <unordered_set>
+#include <cstdio>
+#include <cstring>
+#include <climits>
+#include <cfloat>
+#include <cmath>
+#include <stack>
+
+using namespace std;
+
+int main(){
+	int a, b;
+	cin >> a >> b;
+	cout << a * (b+1) << endl;
+
+	return 0;
+}
