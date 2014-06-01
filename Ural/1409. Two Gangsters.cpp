0bbe8b122e4a1256febf2b0500aab9d67eca7ad8
oliverwreath <oliverwreath@users.noreply.github.com>
2014-06-01 21:31:23 +0800

    Create 1409. Two Gangsters.cpp

---
+++ Ural/1409. Two Gangsters.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cassert>
+#include <sstream>
+#include <algorithm>
+#include <unordered_set>
+#include <cstdio>
+#include <cstring>
+#include <climits>
+#include <cfloat>
+#include <cmath>
+#include <stack>
+
+using namespace std;
+
+int main(){
+	int a, b;
+	cin >> a >> b;
+	cout << b-1 << endl;
+	cout << a-1 << endl;
+
+	return 0;
+}
