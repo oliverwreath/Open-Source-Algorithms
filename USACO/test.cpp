76919042d0926b14eca6b2b5d096df6d8b5ede22
oliverwreath <oliverwreath@users.noreply.github.com>
2014-06-05 22:19:26 +0800

    Create test.cpp

---
+++ USACO/test.cpp
@@ -0,0 +1,33 @@
+/*
+ID: skywalk10
+PROG: test
+LANG: C++11
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cassert>
+#include <sstream>
+#include <algorithm>
+#include <unordered_set>
+#include <cstdio>
+#include <cstring>
+#include <climits>
+#include <cfloat>
+#include <cmath>
+#include <stack>
+#include <queue>
+#include <set>
+#include <list>
+#include <deque>
+
+using namespace std;
+
+int main() {
+    ofstream fout ("test.out");
+    ifstream fin ("test.in");
+    int a, b;
+    fin >> a >> b;
+    fout << a+b << endl;
+    return 0;
+}
