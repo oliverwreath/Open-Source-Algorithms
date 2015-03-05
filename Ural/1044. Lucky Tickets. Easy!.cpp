825ad05b7fda55281aaa9153dcd9e0bf225c2372
oliverwreath <oliverwreath@users.noreply.github.com>
2015-03-05 14:54:45 +0800

    Update and rename Ural to Ural/1044. Lucky Tickets. Easy!.cpp

---
+++ Ural/1044. Lucky Tickets. Easy!.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <set>
+
+using namespace std; 
+
+int main(){
+	int n = 0;
+	cin >> n; 
+	switch(n){
+		case 2: cout << 10 << endl;
+			break; 
+		case 4: cout << 670 << endl;
+			break; 
+		case 6: cout << 55252 << endl;
+			break; 
+		case 8: cout << 4816030 << endl;
+			break; 
+	}
+	
+	return 0;
+}
